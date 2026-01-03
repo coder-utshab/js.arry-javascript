75af1eb8c7134aad0415ded3180147d555ae6fbb
coder-utshab <ghoshutshob470@gmail.com>
2026-01-03 20:43:48 +0530

    up

---
+++ array.js
@@ -0,0 +1,10 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 442;
+// const num4 = 442;
+
+
+const numbers = [12, 22, 54, 65];
+const friends = ['sajib', 'shuvo', 'avijeet','utshab'];
+const passed = [true, false, true, false];
+const mixdArray = [12, 'animesh', true, 45,.566];
