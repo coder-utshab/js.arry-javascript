d1f57e978392fc3d27a3c02a18c7552681c235c5
coder-utshab <ghoshutshob470@gmail.com>
2026-01-03 22:49:26 +0530

    up

---
+++ include.js
@@ -0,0 +1,11 @@
+const friends = ['pranto', 'bitosh', 'piyash', 'amit', 'sujoy'];
+// akhana true false bar korar jonno a vaba korta hoba
+console.log(friends.includes('bitosh'))
+console.log(friends.includes('krishno'))
+
+if(friends.includes('bitosh')){
+    console.log('tonmoy')
+}
+else{
+    console.log('bangladesh is a small contry')
+}
---
+++ indexof.js
@@ -0,0 +1,12 @@
+const friends = ['balam', 'galam', 'salam', 'gelam', 'pailam'];
+
+console.log(friends.indexOf('gelam'));
+
+console.log(friends.indexOf('tomato'));
+
+
+// output
+// PS C:\Users\Utshab Ghosh\Documents\GitHub\js.arry-javascript> node indexof.js
+// 3
+// -1
+// PS C:\Users\Utshab Ghosh\Documents\GitHub\js.arry-javascript> 
